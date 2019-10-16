350e79eb0023c676c259970307b7c99a7db063e7
magizhchai18@gmail.com <magizhchai18@gmail.com>
2019-10-16 19:32:47 +0530

    first code

---
+++ Sample/src/Sample.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+		
+		
+	}
+	
+	}
+		
+	
---
+++ TCS/src/org/test/pom/Base.java
@@ -0,0 +1,10 @@
+package org.test.pom;
+
+public class Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
